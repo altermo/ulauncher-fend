edc20a42f09fe976bd754103149db29c513e74eb
altermo <>
2023-09-09 11:59:22 +0200

    D  .gitignore;D  README.md;D  images/icon.png;A  images/icon.svg;M  main.py;M  manifest.json;M  versions.json

--- main.py
+++ main.py
@@ -1,49 +1,21 @@
-import json
 import logging
-from time import sleep
+import subprocess
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
-from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
+from ulauncher.api.shared.event import KeywordQueryEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
-from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
-from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
-from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
-
-logger = logging.getLogger(__name__)
-
-
-class DemoExtension(Extension):
-
+logger=logging.getLogger(__name__)
+class FendExtension(Extension):
     def __init__(self):
-        super(DemoExtension, self).__init__()
-        self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
-        self.subscribe(ItemEnterEvent, ItemEnterEventListener())
-
-
-class KeywordQueryEventListener(EventListener):
-
-    def on_event(self, event, extension):
-        items = []
-        logger.info('preferences %s' % json.dumps(extension.preferences))
-        for i in range(5):
-            item_name = extension.preferences['item_name']
-            data = {'new_name': '%s %s was clicked' % (item_name, i)}
-            items.append(ExtensionResultItem(icon='images/icon.png',
-                                             name='%s %s' % (item_name, i),
-                                             description='Item description %s' % i,
-                                             on_enter=ExtensionCustomAction(data, keep_app_open=True)))
-
-        return RenderResultListAction(items)
-
-
-class ItemEnterEventListener(EventListener):
-
-    def on_event(self, event, extension):
-        data = event.get_data()
-        return RenderResultListAction([ExtensionResultItem(icon='images/icon.png',
-                                                           name=data['new_name'],
-                                                           on_enter=HideWindowAction())])
-
-
-if __name__ == '__main__':
-    DemoExtension().run()
+        super().__init__()
+        self.subscribe(KeywordQueryEvent,Listen())
+class Listen(EventListener):
+    def on_event(self,event:KeywordQueryEvent,_):
+        args=event.get_argument()
+        out=subprocess.run(['fend',args],capture_output=True).stdout.decode()
+        return [ExtensionResultItem(
+            name=out,
+            on_enter=True,
+        )]
+if __name__=='__main__':
+    FendExtension().run()
